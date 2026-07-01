66cf4465a7a8285b22f11e1c8d658a9bbdf74840
Underworld511 <yliu7949@gmail.com>
2026-07-01 12:19:44 +0800

    chore(cleanup): remove legacy test scaffolding

--- startup.m
+++ startup.m
@@ -5,12 +5,4 @@ function startup()
     addpath(fullfile(rootDir, 'plot'));
     addpath(fullfile(rootDir, 'examples'));
     addpath(fullfile(rootDir, 'tools'));
-    rehash;
-
-    % Clear MATLAB's loaded class cache when startup is rerun in an
-    % existing session after example files have changed shape.
-    try
-        clear classes
-    catch
-    end
 end
